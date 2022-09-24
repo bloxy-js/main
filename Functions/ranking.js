8b654fc909f919383eca82f7fe099f22bb22ec51
svenroblox <111045168+svenroblox@users.noreply.github.com>
2022-09-24 10:12:10 +0930

    Create ranking.js

---
+++ Functions/ranking.js
@@ -0,0 +1,5 @@
+// Used to rank a player.
+
+// Make sure that you have filled out auth.js in YOUR repository you cloned. Read more here: https://forums.thecookie.dev/t/bloxy-js-the-alternative-to-noblox-js/4578
+
+var Auth = "/auth.js"
